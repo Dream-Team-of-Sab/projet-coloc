74843faa5628815f0ee502ea779d34025a8b0e85
Max-AJC-Devops <52999960+Max-AJC-Devops@users.noreply.github.com>
2019-08-23 14:41:28 +0200

    change postgre en sqlite

---
+++ db.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+conn = sqlite3.connect('database_project.db')
+c = conn.cursor()
+
+c.execute ( 
+    '''
+    CREATE TABLE IF NOT EXISTS Users (
+    	id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+	id_colocation INTEGER,
+	    FOREIGN KEY (id_colocation)
+       	    REFERENCES Colocations (id) 
+    );
+    '''
+)
+c.execute (
+    '''
+    CREATE TABLE IF NOT EXISTS Colocations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(255) NOT NULL,
+        address VARCHAR(255) NOT NULL
+    );
+    '''
+)
+c.execute (
+    '''
+    CREATE TABLE IF NOT EXISTS Invoices (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title VARCHAR(255) NOT NULL,
+        price DECIMAL NOT NULL,
+	date DATE NOT NULL,
+	type BOOL NOT NULL,
+	details VARCHAR(255) NOT NULL,
+	id_paying_user INTEGER,
+	    FOREIGN KEY (id_paying_user)
+	    REFERENCES Users (id)
+    );
+    '''
+)
+c.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS Meals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+	date DATE NOT NULL,
+        number FLOAT NOT NULL,
+        id_eating_user INTEGER,
+	    FOREIGN KEY (id_eating_user)
+	    REFERENCES Users (id)
+    );
+    '''
+)
+
+conn.commit()
+c= conn.close()
