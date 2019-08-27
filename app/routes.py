1f5e859be8fa530496a24fea1fd48fad7f48474d
max <ribeiromaxance@gmail.com>
2019-08-27 14:32:45 +0200

    modif

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from app import app
-import psycopg2
+import sqlite3
 import hashlib
 from flask import redirect, render_template, session, url_for, request
 
@@ -29,30 +26,26 @@ def login():
         if request.method == 'GET':
             return render_template('login.html')
         elif request.method == 'POST':
-            conn = psycopg2.connect("dbname=app/app_database.db user=postgres password=postgres")
-            cur = conn.cursor()
-            user_mail_list = [a[0] for a in cur.execute('SELECT email FROM Users').fetchall()]
+            conn = sqlite3.connect('app/app_database.db')
+            c = conn.cursor()
+            user_mail_list = [a[0] for a in c.execute('SELECT email FROM Users').fetchall()]
             if request.form['email'] in user_mail_list:
-                pwd = cur.execute('SELECT password FROM Users WHERE email = %s',\
+                pwd = c.execute('SELECT password FROM Users WHERE email = ?',\
                                 (request.form['email'],))\
                                 .fetchone()[0]
                 if crypted_string(request.form['password']) == pwd:
-                    user_id = cur.execute('SELECT id FROM Users WHERE email = %s',\
+                    user_id = c.execute('SELECT id FROM Users WHERE email = ?',\
                                         (request.form['email'],))\
                                         .fetchone()[0]
-		    cur.close()
                     conn.close()
                     session['logged'] = user_id
                     return redirect(url_for('index'))
                 else:
-		    cur.close()
                     conn.close()
-                    return redirect(url_for('index'))           # Il manque l'affichage  du message d'erreur
-                                                                # coté html
+                    return render_template('login', error = True)
             else:
-		cur.close()
                 conn.close()
-                return redirect(url_for('index'))
+                return render_template('login', error = True)
         else:
             return 'Unknown http method'
 
@@ -70,24 +63,21 @@ def signup():
         conn = sqlite3.connect('app/app_database.db') 
         c = conn.cursor()
         email_list = c.execute('SELECT email FROM Users').fetchone() 
-        if request.form['email'] in email_list : 
+        if request.form['email'] in email_list :                        # Bug ici. Condition jamais rempli. Viens surement de email_list 
             conn.close() 
-            return render_template('sign.html') #, existing_email = True) # Il manque l'affichage du message
-                                                                          # coté html
-        
+            return render_template('sign.html', existing_email = True) 
         else:
             first_name = request.form['first_name'] 
             last_name = request.form['last_name']
             email = request.form['email']
             password = request.form['password']
-            cur.execute('''INSERT INTO Users (first_name, last_name, email, password)
-                         VALUES (%s, %s, %s, %s)''',\
+            c.execute('''INSERT INTO Users (first_name, last_name, email, password)
+                         VALUES (?, ?, ?, ?)''',\
                          (first_name, last_name, email, crypted_string(password))) 
-	    
+
             conn.commit()
-            user_id = cur.execute('SELECT id FROM Users WHERE email = %s', (email,)).fetchone()[0]
+            user_id = c.execute('SELECT id FROM Users WHERE email = ?', (email,)).fetchone()[0]
             session['logged'] = user_id
-	    cur.close()
             c = conn.close() 
             return redirect(url_for('index'))
     else:
@@ -107,23 +97,22 @@ def index():
             return render_template('index.html') 
     
         elif request.method == 'POST':
-            conn = psycopg2.connect("dbname=app/app_database.db user=postgres password=postgres") 
-            cur = conn.cursor()
+            conn = sqlite3.connect('app/app_database.db') 
+            c = conn.cursor()
 
 #Pour l'ajout de facture
-#           invoice_list = cur.execute('SELECT title FROM Invoices').fetchone() 
-#           if request.form['title'] in invoice_list :
-#		cur.close() 
+#           invoice_list = c.execute('SELECT title FROM Invoices').fetchone() 
+#           if request.form['title'] in invoice_list : 
 #               conn.close() 
-#               return render_template('index.html') #, existing_title = True) !! Stopped here!
+#               return render_template('index.html') #, existing_title = True)
 #           else:
             title = request.form['title'] 
             date = request.form['date']
             price = request.form['price']
             details = request.form['details']
-            cur.execute('''INSERT INTO Invoices (title, date, price, details)
-                            VALUES (%s, %s, %s, %s)''', (title, date, price, details)
-               	       )
+            c.execute('''INSERT INTO Invoices (title, date, price, details)
+                            VALUES (?, ?, ?, ?)''', (title, date, price, details)
+                     ) 
             conn.commit()
             return redirect(url_for('index'))
         else:
