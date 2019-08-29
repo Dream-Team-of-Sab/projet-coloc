52fa8ed122338f7ad9ae5601b6bb489b398f7d27
Thomas95i <tom_barbot@hotmail.fr>
2019-08-29 13:31:09 +0000

    function to test connexion to login view added

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,7 +1,2 @@
-#def test_conn(client):
-#    assert client.get('/').status_code == 200
-
-def test_a_la_con():
-    a = 1
-    b = 1
-    assert a + b == 2
+def test_conn(client):
+    assert client.get('/').status_code == 200
