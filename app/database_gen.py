96913df87b6bab235421ec61675b7b8266baa2ec
max <ribeiromaxance@gmail.com>
2019-09-03 11:40:11 +0200

    switch to sqlite3

--- app/database_gen.py
+++ app/database_gen.py
@@ -2,26 +2,26 @@
 '''Create database'''
 # -*- coding: utf-8 -*-
 
-import psycopg2
+import sqlite3
 from functions import crypted_string
 
 #Ouverture connexion
-CONN = psycopg2.connect("host=localhost dbname=app user=app password=app")
+CONN = sqlite3.connect("api_flat.db")
 CUR = CONN.cursor()
 
 CUR.execute(
     '''
     CREATE TABLE IF NOT EXISTS Colocations (
-        id SERIAL PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(255) NOT NULL,
         address VARCHAR(255) NOT NULL
     );
     '''
 )
-CUR.execute( 
-    '''
+CUR.execute(
+    ''' 
     CREATE TABLE IF NOT EXISTS Users (
-    	id SERIAL PRIMARY KEY,
+    	id INTEGER PRIMARY KEY AUTOINCREMENT,
         first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
@@ -35,7 +35,7 @@ CUR.execute(
 CUR.execute(
     '''
     CREATE TABLE IF NOT EXISTS Invoices (
-        id SERIAL PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         title VARCHAR(255) NOT NULL,
         price DECIMAL NOT NULL,
 	type BOOL NOT NULL,
@@ -50,7 +50,7 @@ CUR.execute(
 CUR.execute(
     '''
     CREATE TABLE IF NOT EXISTS Meals (
-        id SERIAL PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
 	date DATE NOT NULL,
         number FLOAT NOT NULL,
         id_eating_user INTEGER,
@@ -61,20 +61,17 @@ CUR.execute(
 )
 #Ajout colocation
 COLOC = ('Coloc', '6 rue de Rougemont, 75000, Paris, France')
-CUR.execute('''INSERT INTO Colocations (name, address) VALUES (%s, %s)''', COLOC)
+CUR.execute('''INSERT INTO Colocations (name, address) VALUES (?, ?)''', COLOC)
 
 #Ajout compte admin
-ADMIN = ('Admin', 'Admin', 'maxanceribeiro@live.fr', crypted_string('072330STM'), 1)
-# ('maxanceribeiro@live.fr', 'maxanceribeiro@live.fr')
-
+ADMIN = ('Admin', 'Admin', 'maxanceribeiro@live.fr', crypted_string('072330STM'), 1, 'maxanceribeiro@live.fr', 'maxanceribeiro@live.fr')
 CUR.execute('''INSERT INTO Users (first_name, last_name, email, password, id_colocation)
-VALUES (%s, %s, %s, %s, %s)''', ADMIN) 
-# WHERE NOT EXISTS (SELECT %s FROM Users WHERE email = %s)''', admin)
+SELECT ?, ?, ?, ?, ? WHERE NOT EXISTS (SELECT ? FROM Users WHERE email = ?)''', ADMIN)
 
 #Ajout fausse facture
 INVOICE = ('Loyer', 29.99, True, '01/01/01', 'details', '1')
-CUR.execute('''INSERT INTO Invoices (title, price, type, date, details, id_paying_user)
-VALUES (%s, %s, %s, %s, %s, %s)''', INVOICE)
+CUR.execute('''INSERT INTO Invoices (title, price, type, date, details, id_paying_user) 
+SELECT ?, ?, ?, ?, ?, ?''', INVOICE)
 
 #Sauvegarde des changements
 CONN.commit()
