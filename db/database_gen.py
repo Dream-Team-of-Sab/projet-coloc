03c71093fe365c0e687ce0c7047872f66c184a92
sabrinatony <sabrinatony74@gmail.com>
2019-09-12 14:19:57 +0200

    Modification design page Add et ajout modal détail facture

--- db/database_gen.py
+++ db/database_gen.py
@@ -14,7 +14,7 @@ CUR.execute(
     CREATE TABLE IF NOT EXISTS Colocations (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(255) NOT NULL,
-        address VARCHAR(255) NOT NULL,
+        adress VARCHAR(255) NOT NULL,
         password VARCHAR(255) UNIQUE NOT NULL
     );
     '''
