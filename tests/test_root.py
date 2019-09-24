54759d162e2cdc4e981f84ece1b282958a6d2a74
max <ribeiromaxance@gmail.com>
2019-09-24 11:48:32 +0200

    add tests

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,2 +1,17 @@
 def test_conn(client):
     assert client.get('/').status_code == 200
+
+def test_log(client):
+    assert client.get('/login/').status_code == 200
+
+def test_sign(client):
+    assert client.get('/signup/').status_code == 200
+
+#def test_index(client):
+#    assert client.get('/index/').status_code == 200
+
+def test_flat(client):
+    assert client.get('/flat/').status_code == 200
+
+#def test_logout(client):
+#    assert client.get('/logout/').status_code == 200
