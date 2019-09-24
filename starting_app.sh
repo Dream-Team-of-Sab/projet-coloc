7fda874a5e86913f196d7300459c159e2a552a6b
max <ribeiromaxance@gmail.com>
2019-09-24 11:20:28 +0200

    deploy automatisation

--- starting_app.sh
+++ starting_app.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+export PATH=~/bin:$PATH
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region eu-west-3
+aws configure set default.output text
+
 aws deploy push --application-name ApiFlat --s3-location s3://app-projet-coloc/ApiFlat.zip
 aws deploy create-deployment --application-name ApiFlat --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name ApiFlat_DepGroup --s3-location bucket=app-projet-coloc,bundleType=zip,key=ApiFlat.zip
