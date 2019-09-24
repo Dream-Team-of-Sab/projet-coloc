0cda08e97b694b951da96b2b8d670281bbdb5aad
max <ribeiromaxance@gmail.com>
2019-09-24 09:50:20 +0200

    test continuous deploy 2

--- starting_app.sh
+++ starting_app.sh
@@ -4,8 +4,8 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 export PATH=~/bin:$PATH
-aws configure set aws_access_key_id ribeiromaxance@gmail.com
-aws configure set aws_secret_access_key 30Septembre1993
+aws configure set aws_access_key_id AKIAQLXGGKZWCNPWYCOA
+aws configure set aws_secret_access_key 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
 aws configure set default.region eu-west-3
 aws configure set default.output text
 
